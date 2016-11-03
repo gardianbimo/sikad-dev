4452119db6840316c3617e6b58718c1f011f5a12
anggakawa <anggakawa@gmail.com>
2016-11-03 16:36:26 +0800

    penambahan namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :m_jurusans
-  resources :m_prodis
-  resources :m_nilais
+  namespace :admin do
+    resources :m_jurusans, :m_prodis, :m_nilais
+  end
+  # resources :m_jurusans
+  # resources :m_prodis
+  # resources :m_nilais
   root 'pages#dashboard'
 end
